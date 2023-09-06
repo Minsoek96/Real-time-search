81f027dd9f9572dcb07d9fac4a929b230647795d
Minsoek96 <fe.minseok96@gmail.com>
2023-09-06 21:31:29 +0900

    fix : Axios 에러로 추정되는 캐싱시 반환값 uppercase 관련 오류 발생 원인 : Axios 를 인터셉트 하더라도 반환값을 config를 주어야 하지만 캐싱된 결과를 반환하여 문제가 됨 해결 : 캐싱 값이 있으면 에러값으로 리턴해주었고,  요청시 에러 객체에서 customData 라는 결과 가 있으면 반환

--- src/api/client.tsx
+++ src/api/client.tsx
@@ -1,21 +1,31 @@
 import axios from "axios";
 import { SERVER } from "./config";
 import { getCacheStorage, setCacheStorage } from "../utils/cacheStorage";
+import { error } from "console";
 
 export const API = axios.create({
   baseURL: `${SERVER}`,
 });
 
-API.interceptors.request.use((config) => {
+API.interceptors.request.use(async (config) => {
   const cacheKey = `${config.method}-${config.url}`;
-  const getCache = getCacheStorage(cacheKey);
+  const getCache = await getCacheStorage(cacheKey);
   if (getCache) {
-    return getCache;
+    return Promise.reject({ customData: getCache });
   }
   return config;
 });
 
-API.interceptors.response.use((response) => {
-  console.log("응답", response);
-  return response;
-});
+API.interceptors.response.use(
+  (response) => {
+    const cachekey = `${response.config.method}-${response.config.url}`;
+    setCacheStorage({ key: cachekey, data: response.data, etime: 1 });
+    return response;
+  },
+  (error) => {
+    if (error.customData) {
+      return error.customData;
+    }
+    return error;
+  }
+);
