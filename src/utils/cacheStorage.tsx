4218b8f092e36d8d50ccd97eeaf9d5e5a5e742a3
Minsoek96 <fe.minseok96@gmail.com>
2023-09-06 19:47:14 +0900

    feat : expire 설정 한 localStorage 캐시기능 구현

---
+++ src/utils/cacheStorage.tsx
@@ -0,0 +1,36 @@
+//input : key , data , expireTime
+//output : cache result
+import { ISick } from "../types";
+interface ICacheItem {
+  key: string;
+  data: ISick[];
+  etime: number;
+}
+
+export const setCacheStorage = ({ key, data, etime }: ICacheItem) => {
+  const expireTimeFormat = etime * 60 * 1000;
+  const item = {
+    sick: data,
+    expiretime: new Date().getTime() + expireTimeFormat,
+  };
+  return localStorage.setItem(key, JSON.stringify(data));
+};
+
+
+export const getCacheStorage = (key: string) => {
+  const getCacheItemsStr = localStorage.getItem(key);
+  const getChacheItem = getCacheItemsStr && JSON.parse(getCacheItemsStr);
+
+  if (!getChacheItem) {
+    return null;
+  }
+
+  const now = new Date().getTime();
+  const expireTime = getChacheItem.expiretime;
+
+  if (now > expireTime) {
+    localStorage.removeItem(key);
+    return null;
+  }
+  return getChacheItem.data;
+};
