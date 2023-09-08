c45e4510ac3c3678aa823659ba45e333ed614704
Minsoek96 <fe.minseok96@gmail.com>
2023-09-08 23:22:30 +0900

    style : 서치 관련 스타일링 진행

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import Search from "../components/Search";
 
 const Home = () => {
-    return (
-        <div>
-            <h5>홈페이지</h5>
-        </div>
-    );
+  return (
+    <S.Wrapper>
+      <S.Banner>
+        <h2>
+          국내 모든 임상시험 검색하고
+          <br />
+          온라인으로 참여하기
+        </h2>
+        <S.Container>
+          <Search />
+        </S.Container>
+      </S.Banner>
+    </S.Wrapper>
+  );
+};
+
+const S = {
+  Wrapper: styled.div`
+    width: 100%;
+  `,
+  Banner: styled.div`
+    background-color: rgb(202, 233, 255);
+    padding: 80px 0px 160px;
+    h2 {
+      text-align: center;
+    }
+  `,
+  Container: styled.div`
+    margin: 0 auto;
+  `,
 };
 
 export default Home;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Search from "../pages/Search";
+import Search from "../components/Search";
 
 const Router = () => {
   return (
