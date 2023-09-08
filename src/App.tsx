78adb8e94f20359194c8abf286a11eb0cc1663a3
Minsoek96 <fe.minseok96@gmail.com>
2023-09-08 23:55:16 +0900

    feat : 배포를 위한 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { worker } from "./mocks/worker";
 import Router from "./router/Router";
-if (process.env.NODE_ENV === "development") {
-  worker.start();
-}
-
+// if (process.env.NODE_ENV === "development") {
+//   worker.start();
+// }
+worker.start();
 function App() {
   return (
     <>
